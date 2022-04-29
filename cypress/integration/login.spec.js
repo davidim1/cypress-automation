8fbb215e12240204c3dc0570daf20b3d26484251
davidim1 <dmdavidmijatovic@gmail.com>
2022-04-29 16:08:19 +0200

    login negative dodan

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -10,6 +10,30 @@ describe('login test', () => {
         cy.get('a[href="/login"]').click();
     })
 
+    it ('login with invalid email', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login')
+        cy.get('#email').type('gugagagagmail.com');
+        cy.get('#password').type('gugagaga1');
+        cy.get('button[type="submit"]').click();
+    })
+
+    it ('login with invalid password', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login')
+        cy.get('#email').type('gugagaga@gmail.com');
+        cy.get('#password').type('gugagaga');
+        cy.get('button[type="submit"]').click();
+    })
+
+    it ('login without click on submit', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login')
+        cy.get('#email').type('gugagaga@gmail.com');
+        cy.get('#password').type('gugagaga1');        
+    })
+
+
     it ('login with valid credentials', () => {
         cy.visit('/login');
         cy.url().should('include', '/login')
